0cf7c7b4ac3bb5cbac3a1f7a0d7c277bc647917d
SIDHARTH KUMTHEKAR <kumthekarsid@gmail.com>
2022-09-19 00:52:15 +0530

    deleted extra files

--- Diggerzz-DAO/public/js/block.js
+++
@@ -1,7 +0,0 @@
-const { ethereum } = window
-const ethereumButton = document.querySelector('.enableEthereumButton');
-
-ethereumButton.addEventListener('click', () => {
-  //Will Start the metamask extension
-  ethereum.request({ method: 'eth_requestAccounts' });
-});
