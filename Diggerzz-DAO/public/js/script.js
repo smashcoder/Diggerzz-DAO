975569555ff159cede24d8435441c60e7de8925f
SIDHARTH KUMTHEKAR <89736536+Sidharth004@users.noreply.github.com>
2022-09-19 00:06:30 +0530

    Whitelist DAPP

---
+++ Diggerzz-DAO/public/js/script.js
@@ -0,0 +1,13 @@
+function myalert(){
+    var txt;
+    if (confirm("You have not Joined DAO.")) {
+      window.location.href='joindao.html'
+    } else {
+      
+    }
+  }
+  function mynotifier(){
+    alert("Coming Soon");
+  }
+
+  
